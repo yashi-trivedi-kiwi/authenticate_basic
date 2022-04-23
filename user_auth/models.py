5a4f7053f3fccb27a30fe596db00ff6c0ceb99ea
Yashi Trivedi <yashi.trivedi@kiwitech.com>
2022-04-23 12:48:41 +0530

    Authentication project added

---
+++ user_auth/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+# from django.forms import ModelForm
+#
+# class Register(models.Model):
+#     first_name = models.CharField(max_length=150, blank=True)
+#     last_name = models.CharField(max_length=150, blank=True)
+#     email = models.EmailField(max_length=150, blank=True)
+#     username = models.CharField(max_length=150, blank=True)
+#     password = models.CharField(max_length=50)
+#     password2 = models.CharField(max_length=50)
+
+
+
